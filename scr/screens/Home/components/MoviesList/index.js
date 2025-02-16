23d26ac036553fe798e37686f4f0d361434fc0be
Pedro Goiânia <pedro.paulods@hotmail.com>
2025-02-16 18:27:01 -0300

    ajustes finais movieList

--- scr/screens/Home/components/MoviesList/index.js
+++ scr/screens/Home/components/MoviesList/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 
-import { FlatList, Image, View } from 'react-native';
-import { Paragraph, Title, TitleSecondary } from '../../../../components/text';
+import { FlatList, View } from 'react-native';
+import { TitleSecondary } from '../../../../components/text';
 import styles from './styles';
 import EmptyList from '../EmptyList';
 import MovieItem from '../MovieItem';
 import Separator from '../../../../components/separator';
 
 function MoviesList({ nowPlayingMovies }) {
-  
   const renderItem = ({ item, index }) => {
     return <MovieItem item={item} index={index} />;
   };
 
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
       <View style={styles.headerContainer}>
         <TitleSecondary>Now Showing</TitleSecondary>
       </View>
@@ -22,7 +21,6 @@ function MoviesList({ nowPlayingMovies }) {
       <FlatList
         horizontal={true}
         showsHorizontalScrollIndicator={false}
-        style={styles.flatListContent}
         ListEmptyComponent={EmptyList}
         ItemSeparatorComponent={Separator({ size: 21, direction: 'horizontal' })}
         ListFooterComponent={Separator({ size: 21, direction: 'vertical' })}
--- scr/screens/Home/components/MoviesList/styles.js
+++ scr/screens/Home/components/MoviesList/styles.js
@@ -1,21 +1,15 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#FFF' },
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
   headerContainer: {
     flexDirection: 'row',
     backgroundColor: 'transparent',
     marginVertical: 16,
     paddingHorizontal: 24,
-  },
-  flatListContent: {
-    flex: 1,
-    padding: 7,
-  },
-
-  buttonsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
   },
 });
 
