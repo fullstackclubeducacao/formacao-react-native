6391a31467d5714cba0a77f3e22ba3f2dff74110
Pedro Goiânia <pedro.paulods@hotmail.com>
2025-06-30 02:45:32 -0300

    feat(release): added changes to publish on google

--- android/app/src/main/java/br/com/myapps/mymovies/MainActivity.kt
+++ android/app/src/main/java/br/com/myapps/mymovies/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.awesomeproject
+package br.com.myapps.mymovies
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/br/com/myapps/mymovies/MainApplication.kt
+++ android/app/src/main/java/br/com/myapps/mymovies/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.awesomeproject
+package br.com.myapps.mymovies
 
 import android.app.Application
 import com.facebook.react.PackageList
